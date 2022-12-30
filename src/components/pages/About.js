e927dbb79865a84e87be0665dec1275c714b0631
Evan Carr <ejrcarr@gmail.com>
2022-12-30 12:15:39 -0500

    Added About section

---
+++ src/components/pages/About.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+	return (
+		<div className='about-container'>
+			<h1 className='about-title'>About RCS</h1>
+			<div className='about-content'>
+				<h2>Our mission</h2>
+				<p>
+					The mission of Roxboro Community School is to achieve and maintain
+					educational excellence by providing a small, inviting and nurturing
+					school that focuses on grades six through twelve. RCS values and
+					respects each member of our school family, thus enabling everyone to
+					become effective, productive citizens. RCS will provide an environment
+					that will direct our students to focus on relevant and rigorous
+					learning that will continue after high school. It is the vision of our
+					school to create educated, responsible and productive men and women
+					who are equipped to face the challenges of the 21st Century.
+				</p>
+				<h2>Our vision</h2>
+				<p>
+					It is the vision of Roxboro Community School to create educated,
+					responsible, and productive men and women who are equipped to face the
+					challenges of the 21st century.
+				</p>
+				<h2>Our core values</h2>
+				<p>
+					Roxboro Community School is committed to fostering a culture of
+					respect that is embedded throughout the school, a culture that is
+					built on the belief that all members are essential to a successful
+					learning environment.
+				</p>
+				<p>
+					Roxboro Community School is committed to a culture of respect which
+					includes fair treatment, honesty, openness, and integrity.
+				</p>
+				<p>
+					Roxboro Community School will maintain high expectations for all
+					students and believe that all students can learn at high levels.
+				</p>
+				<p>We are committed to maintaining:</p>
+				<ul>
+					<li>Trust in each other</li>
+					<li>Recognition of everyone's contributions</li>
+					<li>High expectations for all staff and students</li>
+					<li>Open, honest contributions without fear of retribution</li>
+				</ul>
+				<br />
+				<p>RCS's staff is committed to:</p>
+				<ul>
+					<li>Solving problems, not applying blame</li>
+					<li>Respecting time and the time commitments of others</li>
+					<li>Being aware of nonverbal communications</li>
+					<li>Being an active, responsible member of the RCS team</li>
+					<li>
+						Providing and receiving feedback constructively and respectfully
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default About;
