64f4133ac34447aa602e868e1119121c29725f91
Evan Carr <ejrcarr@gmail.com>
2023-01-06 13:40:52 -0500

    Removed filler experience section homepage

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -6,24 +6,7 @@ export const Main = () => {
 	return (
 		<>
 			<Achievements />
-			<div className='rcs-experience-section'>
-				<div className='home-main-content'>
-					<p>RCS Experience</p>
-					<h1>Be Yourself.</h1>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dicta
-						deleniti culpa sapiente iste! Repudiandae amet voluptatibus tempore
-						at? Libero molestiae doloribus provident?{' '}
-						<span className='bold'>
-							Tempore omnis maxime inventore perspiciatis sapiente autem!
-						</span>
-					</p>
-					<button>
-						Explore <i className='fas fa-long-arrow-alt-right'></i>
-					</button>
-				</div>
-				<div className='home-main-image-gallery'>somethin</div>
-			</div>
+			<div className='rcs-experience-section'></div>
 			<div className='news-section'></div>
 		</>
 	);
