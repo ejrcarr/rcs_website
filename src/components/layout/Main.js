6355a893ffdde4c076d659f33adb481a1fb4fc25
Evan Carr <ejrcarr@gmail.com>
2023-01-07 14:58:04 -0500

    Styled newscards

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Achievements } from '../achievements/Achievements';
 import './HomeMain.css';
+import { NewsCards } from './news/NewsCards';
 
 export const Main = () => {
 	return (
 		<>
 			<Achievements />
-			<div className='rcs-experience-section'></div>
+			<div className='rcs-experience-section'>
+				<h1 className='newscards-title'>News</h1>
+				<NewsCards />
+			</div>
 			<div className='news-section'></div>
 		</>
 	);
